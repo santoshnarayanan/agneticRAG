b17875440b58d4aedf02da6a700ce2ad0fa8e915
Santosh Narayanan <santosh33@gmail.com>
2025-09-01 17:10:28 +0200

    pytest configuration

--- graph/state.py
+++ graph/state.py
@@ -1,1 +1,19 @@
 # This file hols all our graph state
+
+from typing import List, TypedDict
+
+
+class GraphState(TypedDict):
+    """Reprsents the state of the graph
+
+    Args:
+        question:question
+        generation: LLM generation
+        web_search: wether to add search
+        documents: list of documents
+    """
+    question: str
+    generation: str
+    web_search: bool
+    documents: List[str]
+    
