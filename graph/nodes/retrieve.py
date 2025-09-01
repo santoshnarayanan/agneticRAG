ee31d3862286b1a101433eed03824f25095738f4
Santosh Narayanan <santosh33@gmail.com>
2025-09-01 17:17:51 +0200

    fetching nodes for llm graph

---
+++ graph/nodes/retrieve.py
@@ -0,0 +1,9 @@
+from typing import Any, Dict
+
+from  graph.state import GraphState
+from ingestion import retriever
+
+def retrieve(state: GraphState) -> Dict[str, Any]:
+    question = state['question']
+    document = retriever.invoke(question)
+    return {'document': document, 'question': question}
